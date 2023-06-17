d8e0b0f5646d14aec677000a272bda283af2b05d
Alexander Dolbik <alexandr.dolbik@gmail.com>
2023-06-17 13:48:59 +0200

    Functionality update

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ const { info, getInput, error } = require('@actions/core');
 
 const { context: githubContext } = require('@actions/github');
 
-const Bot = require('./bots/bot');
-const FileReview = require('./reviews/file-review');
-const Commenter = require('./github/commenter');
+// const Bot = require('./bots/bot');
+// const FileReview = require('./reviews/file-review');
+// const Commenter = require('./github/commenter');
 
 const run = async ({ fileDiff } = {}) => {
   // run file review
-  info(
+  console.log(
     `githubContext ${JSON.stringify(githubContext)}`
   )
 
-  const repo = githubContext.repo
-  const prNumber = githubContext.payload.pull_request.number
-
-  const bot = new Bot();
-  const fileReview = new FileReview({ bot });
-  const reviews = await fileReview.review({ fileDiff })
-  console.log('!!', reviews);
-
-  const commenter = new Commenter({
-    repo,
-    prNumber,
-    commitId: '1',
-  });
+  // const repo = githubContext.repo
+  // const prNumber = githubContext.payload.pull_request.number
+  //
+  // const bot = new Bot();
+  // const fileReview = new FileReview({ bot });
+  // const reviews = await fileReview.review({ fileDiff })
+  // console.log('!!', reviews);
+  //
+  // const commenter = new Commenter({
+  //   repo,
+  //   prNumber,
+  //   commitId: '1',
+  // });
 }
 
 run();
