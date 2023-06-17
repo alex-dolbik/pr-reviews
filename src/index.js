1099a2d08ddadacc5e61710c65488b410427b562
Alexander Dolbik <alexandr.dolbik@gmail.com>
2023-06-17 12:33:32 +0200

    Functionality update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const { info, getInput, error } = require('@actions/core');
+
+info(`Env VARS: ${JSON.stringify(process.env)}`)
+
 const { context: githubContext } = require('@actions/github');
 
 const Bot = require('./bots/bot');
