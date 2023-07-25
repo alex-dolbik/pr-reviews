48908c0c1386a26f06a4c1d044490a1f2fe37c36
Alexander Dolbik <alexandr.dolbik@gmail.com>
2023-07-25 14:49:40 +0200

    §

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ const { context: githubContext } = require('@actions/github');
 
 const PrReview = require('./reviews/pr-review');
 
+const REVIEW_LABEL = 'ai-review';
+
 const run = async () => {
   // run file review
   // info(
   //   `githubContext ${JSON.stringify(githubContext)}`
   // )
+
+  const prLabels = githubContext.payload.pull_request.labels;
+  const reviewLabel = REVIEW_LABEL;
+  const reviewLabelExists = prLabels.some((label) => label.name === reviewLabel);
+
+  if (!reviewLabelExists) {
+    warning(`Skipped: Review label "${reviewLabel}" is not added`);
+    return;
+  }
 
   if (['pull_request', 'pull_request_target'].includes(githubContext.eventName)) {
     const reviewer = new PrReview(githubContext);
